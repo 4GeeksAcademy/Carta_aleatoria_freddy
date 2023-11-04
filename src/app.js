58fef2e335e38408b410f26ba1375099458b6f16
Freddy2793 <51304209+Freddy2793@users.noreply.github.com>
2023-11-04 14:36:33 +0000

    carta_aleatoria_freddy

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  generar_carta();
 };
+
+function generar_carta() {
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  let numero = [2, 3, 4, 5, 6, 7, 8, 9, "A", "J", "Q", "K"];
+
+  let paloRadom = palos[Math.floor(Math.random() * palos.length)];
+
+  let numeroRadom = numero[Math.floor(Math.random() * palos.length)];
+
+  // console.log(paloRadom, numeroRadom);
+
+  document.getElementById("palo_i").innerHTML = paloRadom;
+
+  document.getElementById("numero").innerHTML = numeroRadom;
+
+  document.getElementById("palo_d").innerHTML = paloRadom;
+
+  if (paloRadom == "♥" || paloRadom == "♦") {
+    document.getElementById("palo_i").style.color = "red";
+
+    document.getElementById("palo_d").style.color = "red";
+  }
+}
+
+setTimeout(generar_carta, 2000);
